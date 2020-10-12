2539012dc6133d32a7c5612748c658aa12deed3d
davidosongschool <60469218+davidosongschool@users.noreply.github.com>
2020-10-12 19:52:44 +0100

    added search function

--- products/views.py
+++ products/views.py
@@ -1,13 +1,28 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 
 # Create your views here.
 def all_products(request):
     """ A view that returns the index page """
     products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You did not search for anything!")
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
+
 
     context = {
         'products': products,
+        'search_term': query 
 
     }
 
