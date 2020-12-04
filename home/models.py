60dcab443662600f75e04f2ba67736faa4290a5c
davidosongschool <60469218+davidosongschool@users.noreply.github.com>
2020-12-04 19:20:02 +0000

    added opening hours model to home app

--- home/models.py
+++ home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class openHours(models.Model):
+
+    class Meta:
+        verbose_name_plural = "Opening Hours"
+
+    day = models.CharField(max_length=254, null=True, blank=True)
+    openingTime = models.TimeField(auto_now=False, null=True)
+    closingTime = models.TimeField(auto_now=False, null=True)
+    
+    def __str__(self):
+        return self.day
--- home/views.py
+++ home/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-
+from .models import openHours
 # Create your views here.
+
 def index(request):
+    hours = openHours.objects.all()
+
+
+
+    context = {
+    'hours': hours
+    }
+
     """ A view that returns the index page """
-    return render(request, 'home/index.html')
+    return render(request, 'home/index.html', context)
