91dc9dcfc47f04ce647d5780f16621a535e007b6
davidosongschool <60469218+davidosongschool@users.noreply.github.com>
2020-12-06 16:08:07 +0000

    created checkout form

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
-
+from .models import Order, OrderLineItem
 # Register your models here.
+
+class OrderLineItemAdminInLine(admin.TabularInline):
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+
+
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderLineItemAdminInLine),
+
+    readonly_fields = ('order_number', 'date', 
+        'delivery_cost', 'subtotal', 
+        'total')
+
+    fields = ('order_number', 'date', 'full_name', 'email',
+               'phone_number', 'street_address1', 'street_address2', 
+               'town_or_city', 'county', 'country', 
+               'postcode', 'delivery_cost',
+               'subtotal', 'total'
+        )
+
+    list_display = ('order_number', 'date', 'full_name',
+                    'subtotal', 'delivery_cost', 'total'
+                    )
+
+    #Put most recent orders at the top of the list
+    ordering = ('-date',)
+
+
+admin.site.register(Order, OrderAdmin)
---
+++ checkout/forms.py
@@ -0,0 +1,35 @@
+from django import forms 
+from .models import Order 
+
+
+class OrderForm(forms.ModelForm):
+    model = Order
+    fields = ('full_name', 'email', 'phone_number',
+              'street_address1', 'street_address2', 
+              'town_or_city', 'postcode', 'county', 'country',)
+
+    def __init__(self, *args, **kwargs):
+        """ Style the form """ 
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'full_name': 'Full Name',
+            'email': 'Email Address',
+            'phone_number': 'Phone Number',
+            'street_address1': 'Street Address 1',
+            'street_address2': 'Street Address 2',
+            'town_or_city': 'Town/City',
+            'postcode': 'Post Code',
+            'county': 'County',
+            'country': 'Country'
+            }
+
+        self.fields['full_name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f' {placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attr['placeholder'] = placeholder
+            self.fields[field].widget.attr['class'] = 'checkout-style-input'
+            self.fields[field].label = False
+
