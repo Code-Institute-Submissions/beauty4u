1899aba7e7ae998353d26360beee639a68231723
unknown <davidosongschool@gmail.com>
2021-01-03 16:42:47 +0000

    added aws config for static files

--- beauty4u/settings.py
+++ beauty4u/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
 
     #other 
     'crispy_forms',
+    'storages',
 
 ]
 
@@ -192,11 +193,26 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
-# Shipping Variables 
+if 'USE_AWS' in os.environ:
+    #Bucket Config
+    AWS_STORAGE_BUCKET_NAME = "beauty4u"
+    AWS_S3_REGION_NAME = "eu-west-1"
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
+    # static files 
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
+    #override static and media URLS in production
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 STANDARD_DELIVERY_COST = 5
-
 #stripe 
 STRIPE_CURRENCY = 'EUR'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
