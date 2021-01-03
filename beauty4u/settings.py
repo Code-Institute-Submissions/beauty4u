ad30cf22fb9d14f14057d40656032aedd6a46995
unknown <davidosongschool@gmail.com>
2021-01-03 22:19:23 +0000

    added gmail config

--- beauty4u/settings.py
+++ beauty4u/settings.py
@@ -119,7 +119,18 @@ AUTHENTICATION_BACKENDS = [
 
 SITE_ID = 1
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' #Log Emails to console 
+
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' #Log Emails to console 
+    DEFAULT_FROM_EMAIL = "beauty4u@example.com"
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_USER')
 
 #User Account Settings
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
