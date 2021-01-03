006959a40d42385216f33f5e40fdb1fda2743970
unknown <davidosongschool@gmail.com>
2021-01-03 10:26:46 +0000

    added user profile update form and auto saved profile information from checkout

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 from .models import UserProfile 
+from .forms import UserProfileForm
 
 
 # Create your views here.
@@ -8,7 +10,17 @@ def profile(request):
 
     profile = get_object_or_404(UserProfile, user=request.user)
 
+    if request.method == "POST":
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request,"Profile Information Updated")
+
+    form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
+
     context = {
-        'profile': profile,
+        'form': form,
+        'orders': orders,
     }
     return render(request, "profiles/profile.html", context)
