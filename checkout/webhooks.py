23982b751d8e4ca18cefb9983a746a5b788e7c3c
unknown <davidosongschool@gmail.com>
2021-01-03 17:13:20 +0000

    added stripe webhook key

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -12,7 +12,7 @@ import stripe
 def webhook(request):
     """Listen for webhooks from Stripe"""
     
-    wh_secret = settings.STRIPE_WH_SECRET
+    wh_secret = settings.STRIPE_WH_KEY
     stripe.api_key = settings.STRIPE_SECRET_KEY
 
     
