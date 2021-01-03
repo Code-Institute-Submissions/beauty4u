06b27bdc66ceb7544fa4027e258160cbec101756
unknown <davidosongschool@gmail.com>
2021-01-03 11:53:04 +0000

    added environment variables

--- beauty4u/settings.py
+++ beauty4u/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&k3_zu*7+xd531g&z!()du6skxx_*jvof5k)cx5i%b@ripj-x*'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['beauty4u.herokuapp.com', 'localhost']
 
