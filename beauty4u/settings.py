bb9ad788c45c9dbecc1c5180695b11e25be45277
unknown <davidosongschool@gmail.com>
2021-01-03 12:36:17 +0000

    debug database connection error

--- beauty4u/settings.py
+++ beauty4u/settings.py
@@ -132,18 +132,15 @@ WSGI_APPLICATION = 'beauty4u.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 
-if 'DATEBASE_URL' in os.environ:
-    DATEBASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'HOST': 'localhost',
-        }
-    }
+}
 
 
 
