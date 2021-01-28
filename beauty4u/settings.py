8e8f9c8fb857e54f9124a91a8153169dedea436a
unknown <davidosongschool@gmail.com>
2021-01-28 23:27:26 +0000

    reconfig of aws static url to fix error

--- beauty4u/settings.py
+++ beauty4u/settings.py
@@ -228,8 +228,8 @@ if 'USE_AWS' in os.environ:
     MEDIAFILES_LOCATION = 'media'
 
     #override static and media URLS in production
-    #STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
-    #MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 
 #stripe 
